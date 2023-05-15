b6b443b4d7eb4cad70e5c714316fd7f4b49605d6
MammadProMax <mhaagrm2@gmail.com>
2023-05-16 03:01:26 +0430

    testing git

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-3xl border overflow-hidden">
           <div className="mb-4">
             <h1 className="text-gray-700 text-3xl font-bold pl-1">Todo List</h1>
+            <p>this is for testing git</p>
             <TodoForm />
           </div>
           <ListContainer />
